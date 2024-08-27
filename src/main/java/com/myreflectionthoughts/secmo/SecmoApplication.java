f47a5c13fd038a579cb39cb443b14da1f0882266
kesrishubham2510 <shubhamkeshari90@gmail.com>
2024-08-27 16:52:47 +0530

    project initialised

---
+++ src/main/java/com/myreflectionthoughts/secmo/SecmoApplication.java
@@ -0,0 +1,13 @@
+package com.myreflectionthoughts.secmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myreflectionthoughts/secmo/SecmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myreflectionthoughts.secmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
